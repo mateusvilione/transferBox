baf5c30280aa890117aba2a3f8122698991194ae
Mateus Vilione <mateusvilione@gmail.com>
2019-11-10 16:16:49 -0300

    pegar a informação do input e criação do state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import "./styles.css";
 
 export default class Main extends Component {
+  state = {
+    newBox: '',
+  };
+  
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.newBox);
+  };
+
+  handleInputChange  = event => {
+    this.setState({ newBox: event.target.value });
+  };
+
   render() {
     return (
         <main id="main-container">
-            <form action="">
+            <form onSubmit={this.handleSubmit}>
                 <img src={logo} alt=""/>
-                <input placeholder="Criar um box" />
+                <input 
+                  placeholder="Criar um box" 
+                  value={this.state.newBox}
+                  onChange={this.handleInputChange}
+                />
                 <button type="submit">Criar</button>
             </form>
         </main>
