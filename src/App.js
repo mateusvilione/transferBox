226261c96bd8341b5b7f2a4301b35d03f2f8e475
Mateus Vilione <mateusvilione@gmail.com>
2019-11-10 17:15:46 -0300

    criação do arquivo routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import Main from "./pages/Main";
 
-
-class App extends Component {
-  render() {
-    return <Main />;
-  }
-}
+const App = () => <Main />
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+);
