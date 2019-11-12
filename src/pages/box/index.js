011066191b27fd8182ba814e9ca5b06055ee6ac0
Mateus Vilione <mateusvilione@gmail.com>
2019-11-12 20:04:34 -0300

    input para subir arquivos

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import api from "../../services/api";
+import { formatDistance } from "date-fns";
+import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
 
 import { MdInsertDriveFile } from 'react-icons/md';
 
@@ -6,35 +10,58 @@ import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 export default class Box extends Component {
+  state = { box: {} };
+  
+  async componentDidMount(){
+    
+    const id = this.props.match.params.id;
+    const response = await api.get(`boxes/${id}`);
+    
+    this.setState({ box: response.data});
+  };
+  
+
+  handleUpload = (files) => {
+    const id = this.props.match.params.id;
+
+    files.forEach( file => {
+      const data = new FormData();
+
+      data.append('file', file);
+      api.post(`boxes/${id}/files`, data);
+    })
+  }
+
   render() {
     return (
       <div id="box-container">
         <header>
           <img src={logo} alt=""/>
-          <h1>Titulo</h1>
+          <h1>{this.state.box.title}</h1>
         </header>
 
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {( {getRootProps, getInputProps}) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Arraste os arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
-          <li>
-            <a className="fileInfo" href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF"/> 
-              <strong>file.pdf</strong>
-            </a>
-
-            <span>há 5 minutos atrás</span>
-          </li>
-
-          <li>
-            <a className="fileInfo" href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF"/> 
-              <strong>file.pdf</strong>
-            </a>
-
-            <span>há 5 minutos atrás</span>
-          </li>
-
+          { this.state.box.files && this.state.box.files.map(file => (
+            <li key={file._id}>
+              <a className="fileInfo" href={file.url} target="_blanck">
+                <MdInsertDriveFile size={24} color="#A5CFFF"/> 
+                <strong>{file.title}</strong>
+              </a>
+              <span> há {" "}
+                {formatDistance(Date.parse(file.createdAt),new Date(),{locale: pt})}
+              </span>
+            </li>
+          )) }
         </ul>
-
       </div>
     );
   }
