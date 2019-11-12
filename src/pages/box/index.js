8693c786700b70dbc3bfef6f3f0502bd733053d6
Mateus Vilione <mateusvilione@gmail.com>
2019-11-12 17:56:53 -0300

    layout do Box

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
+import { MdInsertDriveFile } from 'react-icons/md';
+
+import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 export default class Box extends Component {
   render() {
-    return <div />;
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt=""/>
+          <h1>Titulo</h1>
+        </header>
+
+        <ul>
+          <li>
+            <a className="fileInfo" href="">
+              <MdInsertDriveFile size={24} color="#A5CFFF"/> 
+              <strong>file.pdf</strong>
+            </a>
+
+            <span>há 5 minutos atrás</span>
+          </li>
+
+          <li>
+            <a className="fileInfo" href="">
+              <MdInsertDriveFile size={24} color="#A5CFFF"/> 
+              <strong>file.pdf</strong>
+            </a>
+
+            <span>há 5 minutos atrás</span>
+          </li>
+
+        </ul>
+
+      </div>
+    );
   }
 }
