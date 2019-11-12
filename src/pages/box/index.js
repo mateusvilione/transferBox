ab0cf4c0f3b5b5b61b6c99c322a7bfacc358b740
Mateus Vilione <mateusvilione@gmail.com>
2019-11-12 17:30:31 -0300

    criação da rota box e pages

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import "./styles.css";
 
-export default class box extends Component {
+export default class Box extends Component {
   render() {
     return <div />;
   }
